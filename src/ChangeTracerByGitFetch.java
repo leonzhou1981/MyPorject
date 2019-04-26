40713c4b73a902bfdbeb27a3b2d73bd61789cb43
Zhou Liang <liang.zhou@blujaysolutions.com>
2019-04-26 12:17:40 +0800

    Add Git Change Tracer

--- src/ChangeTracerByGitFetch.java
+++ src/ChangeTracerByGitFetch.java
@@ -17,10 +17,12 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class ChangeTracerByGitFetch {
 
@@ -29,11 +31,11 @@ public class ChangeTracerByGitFetch {
     private static final String BRANCH = "RB_19_0_0";
 
     public static void main(String[] args) {
-        getChangesPerFetch();
-//        getAllJarsPattern();
+        getChangesPerFetch(getAllJarsPattern("C:\\TMFF\\NEW_REPO", true));
     }
 
-    private static void getChangesPerFetch() {
+    private static Map getChangesPerFetch(Map<String, Map> jarsPattern) {
+        Map changeJars = new HashMap();
         Git git = GitUtil.getGit(REMOTE_REPO_NAME, "Portal", BRANCH);
         try {
             CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
@@ -59,35 +61,35 @@ public class ChangeTracerByGitFetch {
                     newTreeIter.reset(reader, newCommit.getTree().getId());
                     List<DiffEntry> diffs = git.diff().setNewTree(newTreeIter).setOldTree(oldTreeIter).call();
                     for (DiffEntry entry : diffs) {
-                        String changeType = entry.getChangeType().name();
                         String oldPath = entry.getOldPath();
                         String newPath = entry.getNewPath();
-                        System.out.println("getChangeType: " + changeType);
-                        System.out.println("getOldPath: " + oldPath);
-                        System.out.println("getNewPath: " + newPath);
+                        for (Iterator jarPatternIter = jarsPattern.keySet().iterator(); jarPatternIter.hasNext();) {
+                            String jarPattern = (String) jarPatternIter.next();
+                            if (oldPath.startsWith(jarPattern) || newPath.startsWith(jarPattern)) {
+                                changeJars.put(jarPattern, jarsPattern.get(jarPattern));
+                            }
+                        }
                     }
                     //merge after fetch
                     git.merge().include(newObjectId).setMessage("Merge Completed").call();
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException | GitAPIException e) {
             e.printStackTrace();
-        } catch (NoHeadException e) {
-            e.printStackTrace();
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-        } finally {
-
         }
+
+        return changeJars;
     }
 
-    private static void getAllJarsPattern() {
-        final String[] dirs = new String[]{
-            "C:\\TMFF\\NEW_REPO",
-            "D:\\KFF_HEAD"
-        };
+    private static Map jarsPatternCache = null;
 
-        final String[] suffixes = new String[]{
+    private static Map getAllJarsPattern(final String projectDirectory, boolean useJarsPatternCache) {
+
+        if (useJarsPatternCache && jarsPatternCache != null) {
+            return jarsPatternCache;
+        }
+
+        final String[] customizedJarSuffixes = new String[]{
             "-form-business",
             "-form-common",
             "-form-desktop",
@@ -99,93 +101,72 @@ public class ChangeTracerByGitFetch {
             "-report-desktop"
         };
 
-        final StringBuffer svnjars = new StringBuffer("|");
-        for (int i = 0; i < dirs.length; i++) {
-            final int index = i;
-            final int[] counter = {0};
-            FileUtil.iterateAllFilesUnderOneDirectory(dirs[i], new IFileAction() {
-                public void doFileProcess(File file) {
-                    try {
-                        if (isPOM(file)) {
-                            Document doc = XMLUtil.readXMLFile(file);
-                            String path = file.getAbsolutePath().substring(dirs[index].length());
-                            path = path.substring(0, path.length() - "pom.xml".length());
-                            path = path.replace("\\", "/");
-                            if (path != null && !(path.startsWith(File.separator + "Deployment" + File.separator)
-                                || path.startsWith(File.separator + "kff" + File.separator + "RegressionTest" + File.separator))) {
-                                Element root = doc.getDocumentElement();
-                                if (root != null) {
-                                    String packaging = null;
-                                    String artifactId = null;
-                                    String groupId = null;
-                                    String forwho = null;//for which customer
-                                    NodeList nodes = root.getChildNodes();
-                                    if (nodes != null && nodes.getLength() > 0) {
-                                        for (int i = 0; i < nodes.getLength(); i++) {
-                                            Node node = nodes.item(i);
-                                            if (Node.ELEMENT_NODE == node.getNodeType()) {
-                                                if ("packaging".equals(node.getNodeName())) {
-                                                    packaging = node.getFirstChild().getNodeValue();
-                                                }
-                                                if ("artifactId".equals(node.getNodeName())) {
-                                                    artifactId = node.getFirstChild().getNodeValue();
-                                                    for (int k = 0; k < suffixes.length; k++) {
-                                                        forwho = StringUtil.pickupWord(artifactId, "kewill-kff-", suffixes[k]);
-                                                        if (forwho != null) {
-                                                            break;
-                                                        }
+        final Map<String, Map> jarsPattern = new HashMap();
+
+        FileUtil.iterateAllFilesUnderOneDirectory(projectDirectory, new IFileAction() {
+            public void doFileProcess(File file) {
+                try {
+                    if (isPOM(file)) {
+                        Document doc = XMLUtil.readXMLFile(file);
+                        String path = file.getAbsolutePath().substring(projectDirectory.length());
+                        path = path.substring(0, path.length() - "pom.xml".length());
+                        path = path.replace("\\", "/");
+                        if (path != null && !(path.startsWith(File.separator + "Deployment" + File.separator)
+                            || path.startsWith(File.separator + "kff" + File.separator + "RegressionTest" + File.separator))) {
+                            Element root = doc.getDocumentElement();
+                            if (root != null) {
+                                String packaging = null;
+                                String artifactId = null;
+                                String groupId = null;
+                                String forwho = null;//for which customer
+                                NodeList nodes = root.getChildNodes();
+                                if (nodes != null && nodes.getLength() > 0) {
+                                    for (int i = 0; i < nodes.getLength(); i++) {
+                                        Node node = nodes.item(i);
+                                        if (Node.ELEMENT_NODE == node.getNodeType()) {
+                                            if ("packaging".equals(node.getNodeName())) {
+                                                packaging = node.getFirstChild().getNodeValue();
+                                            }
+                                            if ("artifactId".equals(node.getNodeName())) {
+                                                artifactId = node.getFirstChild().getNodeValue();
+                                                for (int k = 0; k < customizedJarSuffixes.length; k++) {
+                                                    forwho = StringUtil.pickupWord(artifactId, "kewill-kff-", customizedJarSuffixes[k]);
+                                                    if (forwho != null) {
+                                                        break;
                                                     }
                                                 }
-                                                if ("groupId".equals(node.getNodeName())) {
-                                                    groupId = node.getFirstChild().getNodeValue();
-                                                }
-
                                             }
+                                            if ("groupId".equals(node.getNodeName())) {
+                                                groupId = node.getFirstChild().getNodeValue();
+                                            }
+
                                         }
-    //                                    if ("jar".equals(packaging) || null == packaging) {
-                                        if ("jar".equals(packaging)) {
-                                            if (forwho == null || "standard".equals(forwho) || "utilities".equals(forwho) || "product".equals(forwho)) {
-                                                if (index == 0) {
-                                                    svnjars.append(groupId).append("|").append(artifactId).append("|");
-
-                                                } else {
-                                                    if (!svnjars.toString().contains("|" + groupId + "|" + artifactId + "|")) {
-                                                        System.out.println(groupId);
-                                                        System.out.println(artifactId);
-                                                        System.out.println(path);
-                                                        System.out.println("---------------------------------------------------------");
-                                                    }
-                                                }
-                                                /*System.out.println(groupId);
-                                                System.out.println(artifactId);
-                                                System.out.println(path);
-                                                System.out.println("---------------------------------------------------------");*/
-                                                counter[0]++;
-                                            } else {
-                                                //customized jars
-                                            }
+                                    }
+                                    if ("jar".equals(packaging)) {
+                                        Map mvnMap = new HashMap();
+                                        mvnMap.put("groupId", groupId);
+                                        mvnMap.put("artifactId", artifactId);
+                                        mvnMap.put("path", path);
+                                        jarsPattern.put(path, mvnMap);
+                                        if (forwho == null || "standard".equals(forwho) || "utilities".equals(forwho) || "product".equals(forwho)) {
+                                            //standard jars
+                                        } else {
+                                            //customized jars
                                         }
                                     }
                                 }
                             }
                         }
-                    } catch (FileNotFoundException e) {
-
-                    } catch (IOException e) {
-
-                    } catch (ParserConfigurationException e) {
-
-                    } catch (SAXException e) {
-
-                    } finally {
-
                     }
+                } catch (IOException | ParserConfigurationException | SAXException e) {
+                    e.printStackTrace();
                 }
-            });
-            System.out.println(dirs[i] + " has " + counter[0] + " jars ");
-        }
-
+            }
+        });
+        jarsPatternCache = jarsPattern;
+        return jarsPattern;
     }
+
 
     private static boolean isPOM(File file) {
         return "pom.xml".equals(file.getName());
